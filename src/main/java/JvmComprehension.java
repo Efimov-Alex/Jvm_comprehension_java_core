1c8c6a73ef617120f05174d12e97f67fdbb33d26
Efimov-Alex <fastic2018l@yandex.ru>
2023-07-07 13:08:30 +0300

    commit 1

--- src/main/java/JvmComprehension.java
+++ src/main/java/JvmComprehension.java
@@ -14,12 +14,12 @@ public class JvmComprehension {
     }
 }
 /*
-1 - загрузчик классов для JvmComprehension, int, создаётся фрейм main в стеке, а в нем int i = 1. В метаспейс -
-JvmComprehension.class.
-2 - загрузчик классов для Object, в куче - Object, в фрейме main obj.
-3 - загрузчик классов для Integer, в куче - Integer, в фрейме main Integer ii = 2.
-4 - создаётся фрейм в стеке printAll. В этом фрейме появляются Object o, int i, Integer ii.
+Сначала создаётся фрейм main в Stack Memory
+1 - в фрейме main int i = 1. В Metaspace - JvmComprehension.class.
+2 - в Heap - Object, в фрейме main o.
+3 - в Heap - Integer, в фрейме main Integer ii = 2.
+4 - создаётся фрейм в Stack Memory printAll. В этом фрейме появляются Object o, int i, Integer ii.
 5 - в фрейме printAll  Integer uselessVar = 700.
-6 - Создастся новый фрейм в стеке, куда передадим ссылку на o, i, i1. Сборщик мусора для uselessVar.
-7 - Создастся новый фрейм в стеке. Сборщик мусора для o, i, i1.
+6 - Создастся новый фрейм в Stack Memory, куда передадим ссылку на o, i, i1. Сборщик мусора для uselessVar.
+7 - Создастся новый фрейм в Stack Memory.
  */
